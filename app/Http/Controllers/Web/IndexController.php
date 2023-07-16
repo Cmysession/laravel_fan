0554ea5b0d21eb1905dbff6221484c381ba40ae6
ajsd <xxxx@example.com>
2023-07-16 22:29:59 +0800

    404

--- app/Http/Controllers/Web/IndexController.php
+++ app/Http/Controllers/Web/IndexController.php
@@ -3,25 +3,65 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
 
-    public function index()
+    public $template = 'm_1';
+
+
+    public function __construct()
     {
-        $tmp = 'm_1';
-        $index_html = file_get_contents(storage_path("emp/{$tmp}/index.html"));
+
+    }
+
+
+    public function index(): void
+    {
+        $index_html = @file_get_contents(storage_path("template/$this->template/index.html"));
+        if (!$index_html) {
+            die("<h2 style='text-align: center'> index.html </h2>");
+        }
+        die($this->exchange($index_html));
+    }
+
+    public function list(): void
+    {
+
+        $index_html = @file_get_contents(storage_path("template/$this->template/list.html"));
+        if (!$index_html) {
+            die("<h2 style='text-align: center'> list.html </h2>");
+        }
         die($index_html);
     }
 
-    public function list(Request $request)
+    public function row(): void
     {
-        die('list');
+
+        $index_html = @file_get_contents(storage_path("template/$this->template/row.html"));
+        if (!$index_html) {
+            die("<h2 style='text-align: center'> row.html </h2>");
+        }
+        die($index_html);
     }
 
-    public function row(Request $request)
+    public function exchange(string $html): string
     {
-        die('row');
+
+        $title_str = "{标题}";
+        // 出现了几次
+        $title_str_count = substr_count($html, $title_str);
+        $title_file = @file_get_contents(storage_path("template/$this->template/key/t.txt"));
+        if (!$title_file){
+            die("<h2 style='text-align: center'> t.txt</h2>");
+        }
+        dump($title_file);
+        // 替换
+        for ($i = 0; $i < $title_str_count; $i++) {
+            $html = preg_replace("/$title_str/", '34', $html, 1);
+        }
+//        var_dump($html);
+        return $html;
     }
+
 }
