1b48daa6d50056134c366cbe753c3539883d88c5
ajsd <xxxx@example.com>
2023-07-30 16:21:44 +0800

    conf

--- config/web_demo.php
+++ config/web_demo.php
@@ -1,15 +1,29 @@
 <?php
 return [
     'yzy345.com' => [
+        // 网站相关
         'template' => 'wdj',//模板
         'prefix_status' => 0,//泛解析 1开,0关
         'prefix_path_status' => 1,//泛目录 1开,0关
-        'cache_path' => 0,//缓存 1开,0关  蜘蛛模式才生效
+        'cache_path' => 1, //缓存 1开,0关 // 蜘蛛模式才生效
+        // 百度推送相关
+        'baidu_status' => 0, //是否推送 1开,0关
+        'baidu_number' => 10, //每日推送条数
+        'baidu_site' => 'www.yzy345.com', // 推送域名 www
+        'baidu_token' => 'token', //推送 token
+        // 跳转相关
+        'is_jump' => 0, // 1跳,0关
+        '跳转状态' => 200, // 200, 302, 404
+        '跳转地址' => '', // 完整地址 http | https
     ],
     '127.0.0.1' => [
         'template' => 'wdj',//模板
         'prefix_status' => 1,//泛解析 1开,0关
         'prefix_path_status' => 1,//泛目录 1开,0关
         'cache_path' => 0,//缓存 1开,0关  蜘蛛模式才生效
+        'baidu_status' => 0, //是否推送
+        'baidu_number' => 10, //每日推送条数
+        'baidu_site' => '127.0.0.1', // 推送域名 www
+        'baidu_token' => 'token', //推送token
     ]
 ];
