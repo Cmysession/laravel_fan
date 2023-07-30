908ff1ed9aa3b37e06b9f92bb8dbcae83025b704
ajsd <xxxx@example.com>
2023-07-30 21:54:02 +0800

    arra

--- config/web_demo.php
+++ config/web_demo.php
@@ -16,14 +16,4 @@ return [
         '跳转状态' => 200, // 200, 302, 404
         '跳转地址' => '', // 完整地址 http | https
     ],
-    '127.0.0.1' => [
-        'template' => 'wdj',//模板
-        'prefix_status' => 1,//泛解析 1开,0关
-        'prefix_path_status' => 1,//泛目录 1开,0关
-        'cache_path' => 0,//缓存 1开,0关  蜘蛛模式才生效
-        'baidu_status' => 0, //是否推送
-        'baidu_number' => 10, //每日推送条数
-        'baidu_site' => '127.0.0.1', // 推送域名 www
-        'baidu_token' => 'token', //推送token
-    ]
 ];
