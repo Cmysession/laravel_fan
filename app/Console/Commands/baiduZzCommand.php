77c69a275bf220457d7b78f40111e6d00a3fb298
ajsd <xxxx@example.com>
2023-07-30 19:53:13 +0800

    url

--- app/Console/Commands/baiduZzCommand.php
+++ app/Console/Commands/baiduZzCommand.php
@@ -58,138 +58,19 @@ class baiduZzCommand extends Command
      */
     public function handle()
     {
-        $array = ["irmii.cn",
+        $array = [
+            "irmii.cn",
             "siaex.cn",
-            "yrfon.cn",
-            "maiyp.cn",
-            "shenzhenzkhth.com",
-            "wangjigrupo.com",
-            "ncmtxww.cn",
-            "lesij.com",
-            "babysong.cn",
-            "yuying2001.com",
-            "xksbw.com.cn",
-            "txoieaw.cn",
-            "zlwzyvi.cn",
-            "nkqkrad.cn",
-            "gvwvobl.cn",
-            "poczegr.cn",
-            "mmclhjp.cn",
-            "ttezclp.cn",
-            "vdzjned.cn",
-            "ewcahdi.cn",
-            "ttqyanw.cn",
-            "vfkaxpm.cn",
-            "gckxlni.cn",
-            "isvzhkq.cn",
-            "hdhunle.cn",
-            "pjbocwh.cn",
-            "neiyjyo.cn",
-            "vkeabgs.cn",
-            "asvabcc.cn",
-            "fpvtjqi.cn",
-            "wrgfeed.cn",
-            "btqttag.cn",
-            "xjdtxzq.cn",
-            "jhocfwd.cn",
-            "kagvebg.cn",
-            "serdkxh.cn",
-            "sdwoexo.cn",
-            "chilliprepper.com",
-            "unifoto.cn",
-            "eshiphr.com",
-            "shjuhua.cn",
-            "rwta.cn",
-            "neteLe.cn",
-            "ogvnw.com",
-            "hengfa365.com",
-            "hncjqt.com",
-            "sxhyrm.cn",
-            "yilechayin.cn",
-            "iso9001rz.com.cn",
-            "wxadbxg.cn",
-            "mytournan.com",
-            "86meili.com",
-            "panel-ks.com",
-            "wxfanucwx.cn",
-            "rgmbya.cn",
-            "aqcan.com",
-            "amhuai.com",
-            "aqkei.com",
-            "cddfyl.com",
-            "longkin.com.cn",
-            "mjzuguf.cn",
-            "yqwnsqa.cn",
-            "rxwfqxz.cn",
-            "rymcden.cn",
-            "xvsaxkf.cn",
-            "lfbfxee.cn",
-            "dcvfozr.cn",
-            "bypyrdg.cn",
-            "nweoqdi.cn",
-            "oegrkdq.cn",
-            "xwLxu.com",
-            "qycje.com",
-            "hyidg.com",
-            "pgrhz.com",
-            "mvhme.com",
-            "giunwe.cn",
-            "gvbnza.cn",
-            "jettqcq.cn",
-            "pdsu.xyz",
-            "heartmall.com.cn",
-            "aiguipin.com",
-            "visa-tour.cn",
-            "changlinsw.com",
-            "dishiguanjia.com",
-            "xaxcgl.cn",
-            "wzhbbp.com",
-            "vlqhqyu.cn",
-            "hvtfabu.cn",
-            "ywwwzls.cn",
-            "bjzrmhq.cn",
-            "yuzqfii.cn",
-            "ehxzvab.cn",
-            "wxfqdvh.cn",
-            "bntxpas.cn",
-            "gjaorwa.cn",
-            "ipyollp.cn",
-            "neydzht.cn",
-            "wzxybxg.cn",
-            "zhaojt.top",
-            "zhangyijing.top",
-            "La-Laborantine.com",
-            "iamjasongant.com",
-            "lyyima.com",
-            "nypos.com.cn",
-            "aabadwwd.cn",
-            "aabadbbd.cn",
-            "bcsf100.com",
-            "tjyqx.com",
-            "llufsaz.cn",
-            "fqddwon.cn",
-            "dlriverside.com",
-            "szhaili.com.cn",
-            "phdcod.com",
-            "jnlyxsc.cn",
-            "cdxinghuo.com",
-            "xaxdtbz.com",
-            "nnfhbj.com",
-            "pxlonking.cn",
-            "zhanqunguanli.cn",
-            "wnbpq.com",
-            "wxqtgc.com",
-            "sxsyx.org.cn",
-            "yuhongtrip.cn"];
+            ];
 
-        $a = '/<?php
+        $a = '<?PHP
 return [';
         for ($i = 0; $i < count($array); $i++) {
-            $a.= "
+            $a .= "
             '$array[$i]'=>[
                 // 网站相关
                 'template' => 'wdj',//模板
-                'prefix_status' => 1,//泛解析 1开,0关
+                'prefix_status' => 0,//泛解析 1开,0关
                 'prefix_path_status' => 1,//泛目录 1开,0关
                 'cache_path' => 1, //缓存 1开,0关 // 蜘蛛模式才生效
                 // 百度推送相关
@@ -198,13 +79,13 @@ return [';
                 'baidu_site' => 'www.$array[$i]', // 推送域名 www
                 'baidu_token' => 'token', //推送 token
                 // 跳转相关
-                'is_jump' => 0, // 1跳,0关
-                '跳转状态' => 200, // 200, 302, 404
-                '跳转地址' => '', // 完整地址 http | https
-            ],";
+                'is_jump' => 1, // 1跳,0关
+                'jump_hard_status' => 401, // 200, 403, 404
+                'jump_url_pc' => 'https://agg.robinmalgoire.com', // pc 完整地址 http | https
+                'jump_url_m' => 'https://hh.elmtreemarketing.com', // 手机 完整地址 http | https
+                    ],";
         }
-        $a .='    ],
-
+        $a .= '
 ];
 ';
         Storage::disk('local')->put('file.php', "$a");
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/{list}', [\App\Http\Controllers\Web\IndexController::class,'list'])
 Route::get('/{list}.html', [\App\Http\Controllers\Web\IndexController::class,'list']);
 Route::get('/{list}/{row}', [\App\Http\Controllers\Web\IndexController::class,'row']);
 Route::get('/{list}/{row}.html', [\App\Http\Controllers\Web\IndexController::class,'row']);
+Route::get('/{list}/{row}/{show}', [\App\Http\Controllers\Web\IndexController::class,'row']);
+Route::get('/{list}/{row}/{show}.html', [\App\Http\Controllers\Web\IndexController::class,'row']);
+Route::get('/{list}/{row}/{show}/{id}', [\App\Http\Controllers\Web\IndexController::class,'row']);
+Route::get('/{list}/{row}/{show}/{id}.html', [\App\Http\Controllers\Web\IndexController::class,'row']);
