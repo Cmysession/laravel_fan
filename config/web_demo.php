f88cbac8ed530f5be73fb71aee7d28a30a1bd7ee
ajsd <xxxx@example.com>
2023-09-05 16:09:23 +0800

    conf

--- config/web_demo.php
+++ config/web_demo.php
@@ -4,13 +4,14 @@ return [
         // 网站相关
         'template' => 'wdj',//模板
         'prefix_status' => 1,//泛解析 1开,0关
-        'prefix_path_status' => 1,//泛目录 1开,0关
+        'prefix_path_status' => 0,//泛目录 1开,0关
         'cache_path' => 1, //缓存 1开,0关 // 蜘蛛模式才生效
+        'to_asc' => 1, //字符转义 1开,0关
 
         // 百度推送相关
         'baidu_status' => 0, //是否推送 1开,0关
-        'baidu_number' => 0, //每日推送条数
-        'baidu_token' => '', //推送 token
+        'baidu_number' => 10, //每日推送条数
+        'baidu_token' => 'token', //推送 token
         'prefix_site_array' => [  // 泛前缀  泛前缀 -- 百度推送需要 默认www
 
         ],
@@ -21,4 +22,5 @@ return [
         'jump_url_pc' => 'https://agg.robinmalgoire.com', // pc 完整地址 http | https
         'jump_url_m' => 'https://hh.elmtreemarketing.com', // 手机 完整地址 http | https
     ],
+
 ];
